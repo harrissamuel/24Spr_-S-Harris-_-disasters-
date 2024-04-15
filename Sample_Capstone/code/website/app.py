5cb1e5ad8c4a8a8eb683c0063d891e080c35fbad
Samuel Harris <harrissamuel130@gmail.com>
2024-04-14 23:09:11 -0400

    update

--- Sample_Capstone/code/website/app.py
+++ Sample_Capstone/code/website/app.py
@@ -6,6 +6,20 @@ def display_html(html_file_path):
     if os.path.exists(html_file_path):
         with open(html_file_path, "r") as f:
             plotly_map_html = f.read()
+        # Apply custom CSS to remove white space
+        st.markdown(
+            f"""
+            <style>
+                .stComponent.e1e1i5i31 {{
+                    width: 100%;
+                    height: 100%;
+                    margin: 0;
+                    padding: 0;
+                }}
+            </style>
+            """,
+            unsafe_allow_html=True,
+        )
         st.components.v1.html(plotly_map_html, width=800, height=600)
     else:
         st.error("HTML file not found. Please make sure it exists in the same directory as your app.py.")
@@ -20,8 +34,13 @@ if selected_tab == "Tab 1":
     st.title("General")
     st.write("This is the content of Tab 1.")
 
+    st.write("The following bar chart shows significant deaths by year and region. Specifcally, \
+            it highlights large mass casualty events in both the Americas and Asia.")
     # Plot 1: Bar chart of deaths by year and region
     display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/stacked_bar_chart_of_deaths_by_year_and_region.html")
+
+    st.write("Next, the following map shows disaster deaths by country and year. In addition, it highlights the disaster \
+             type that caused the most deaths")
 
     # Plot 2: Deaths Map with year slider
     display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/deaths_map_with_year_slider")
@@ -48,17 +67,32 @@ elif selected_tab == "Tab 2":
     # Plot 6: Box plot of Deaths by region (americas)
     display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/Violinplot_of_Total Deaths_by_Subregion(americas)_(without_outliers)")
 
+    # Plot 7: Deaths by Month
+    display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/disasters_by_month")
+
+    # Plot 8: Lengths of Disaster
+    display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/length_of_disaster_plot")
+
 elif selected_tab == "Tab 3":
     st.title("World Bank Disaster Analysis")
     st.write("This is the content of Tab 3.")
-    # Plot 1: Disasters by Development Map
+    # Plot 1: Individual Variables with Death
+    display_html('/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/Subplots_WB')
+
+    # Plot 2: Disasters by Development Map
     display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/development_map_with_tab.html")
 
-    # Plot 2: Deaths by Development Type and Disaster
+    # Plot 3: Deaths by Development Type and Disaster
     display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/bar_chart_with_development_category_and_disaster_type.html")
 
-    # Plot 3: Timeline of Deaths by development Type
+    # Plot 4: Timeline of Deaths by development Type
     display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/line_plot_with_start_year_and_development_category.html")
 
-    # Plot 4: Risk Score
+    # Plot 5: Number of disasters by development type
+    display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/number_of_disaster_development_category.html")
+
+    # Plot 6: Median Deaths by Development scores 
+    display_html('/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/median_total_deaths_by_dev_score.html')
+
+    # Plot 7: Risk Score
     display_html("/Users/samharris/Desktop/Capstone/24Spr_-S-Harris-_-disasters-/Sample_Capstone/code/website/risk_score_map.html")
